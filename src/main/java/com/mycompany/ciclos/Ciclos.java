d54a9b3e91eb98017fa41a69b9a4168256f112cc
jhonpraxedes <jhonpraxedes@SALAZAR.SALAZAR>
2022-12-15 09:20:11 -0600

    ciclo for y while

---
+++ src/main/java/com/mycompany/ciclos/Ciclos.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ciclos;
+import java.util.Scanner;
+/**
+ *
+ * @author jhonpraxedes
+ */
+public class Ciclos {
+
+    public static void main(String[] args) {
+        
+        int i;
+        for(i=1;i<11;i++){
+            System.out.println("usando for"+i);
+           
+        }
+        i=1;
+        do{
+            System.out.println("usando WHILE"+i);
+            i++;
+        }while(i<11);
+        
+    }
+}
